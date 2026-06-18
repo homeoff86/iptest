fdd1128a8b30cf6a74d1145d3aae10653a8f6471
homeoff86 <honeoff066@gmail.com>
2026-06-19 03:12:02 +0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-// Add the map tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
